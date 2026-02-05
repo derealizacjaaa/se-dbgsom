63de078fb890421e36eddaffb1c4ca34b1490c1e
derealizacjaaa <max.okumski@gmail.com>
2026-02-05 16:33:52 +0100

    Delete repro_crash.py

--- repro_crash.py
+++
@@ -1,8 +0,0 @@
-
-try:
-    print("Importing juliacall...")
-    from juliacall import Main as jl
-    print("juliacall imported successfully.")
-    print("Julia version:", jl.seval("VERSION"))
-except Exception as e:
-    print("Error importing juliacall:", e)
